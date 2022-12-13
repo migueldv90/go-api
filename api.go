4e75a0706a39df5274ddad71e6a9eb1de6b1faf3
Miguel Vazquez <migueldv90@yahoo.com>
2022-12-12 23:04:39 -0600

    update

--- api.go
+++ api.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, World")
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	http.HandleFunc("/hello", helloHandler)
+	http.ListenAndServe(":8080", nil)
 }
